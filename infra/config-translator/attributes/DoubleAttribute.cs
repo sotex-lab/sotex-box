dafa060b955c8d29ddea64fd0708e3a9298f40e8
nikolamilosa <nikolamilosa20@gmail.com>
2024-02-26 09:57:04 +0100

    removing leftover code

--- infra/config-translator/attributes/DoubleAttribute.cs
+++ infra/config-translator/attributes/DoubleAttribute.cs
@@ -7,15 +7,6 @@ public class DoubleAttribute : GenericAttribute<double>
     public DoubleAttribute(string name, bool isList = false, bool isSecret = false)
         : base(name, isList, isSecret) { }
 
-    public override object? GetConfigValue(Config config) =>
-        (IsList, IsSecret) switch
-        {
-            (true, true) => config.RequireSecretObject<IEnumerable<double>>(Name),
-            (true, false) => config.RequireObject<IEnumerable<double>>(Name),
-            (false, true) => config.RequireSecretDouble(Name),
-            (false, false) => config.RequireDouble(Name),
-        };
-
     protected override object? GetSpecificObject(Config config) =>
         IsSecret switch
         {
