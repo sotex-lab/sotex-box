0d3adcc1d09fc308ac2d0c3fae9ce0b89b6197ba
AleksaBajat <aleksabajat2001@gmail.com>
2024-03-28 12:46:21 +0100

    Added splash screen

--- android/launcher/android/app/src/main/kotlin/com/example/launcher/MainActivity.kt
+++ android/launcher/android/app/src/main/kotlin/com/example/launcher/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.launcher
 import io.flutter.embedding.android.FlutterActivity
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode.transparent
+import org.devio.flutter.splashscreen.SplashScreen
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        intent.putExtra("background_mode", transparent.toString())
+        SplashScreen.show(this, true)
+        // intent.putExtra("background_mode", transparent.toString())
         super.onCreate(savedInstanceState)
     }
 }
