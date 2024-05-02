da4a097b58ea644389b2835e51fbbabd11d5be50
AleksaBajat <aleksabajat2001@gmail.com>
2024-05-02 21:48:00 +0200

    clumsy: Leftover removal.

--- android/launcher/lib/src/sse/sse_entry.dart
+++ android/launcher/lib/src/sse/sse_entry.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
-import 'dart:isolate';
 import 'package:eventflux/client.dart';
 import 'package:eventflux/enum.dart';
 import 'package:eventflux/models/response.dart';
-import 'package:flutter/services.dart';
 import 'package:launcher/src/common/logging.dart';
 import 'package:launcher/src/sse/processing/processor.dart';
 import 'package:launcher/src/sse/processing/processor_factory.dart';
-import 'package:tuple/tuple.dart';
 
 extension SSEConverting on String {
   SSE toSSE() {
@@ -29,58 +26,6 @@ class SSEUndefinedMessage implements SSE {}
 class SSENoopMessage implements SSE {}
 
 class SSEScheduleMessage implements SSE {}
-
-Future<RootIsolateToken> waitForRootToken(ReceivePort receivePort) {
-  Completer<RootIsolateToken> completer = Completer<RootIsolateToken>();
-
-  receivePort.listen((message) {
-    if (message is RootIsolateToken) {
-      completer.complete(message);
-    }
-  });
-
-  return completer.future;
-}
-
-Future<SSE> waitForSSE(ReceivePort receivePort) {
-  Completer<SSE> completer = Completer<SSE>();
-
-  receivePort.listen((message) {
-    if (message is SSE) {
-      completer.complete(message);
-    }
-  });
-
-  return completer.future;
-}
-
-Future<Tuple2<RootIsolateToken, SSE>> waitForTokenAndSSE(
-    ReceivePort receivePort) async {
-  var tokenCompleter = Completer<RootIsolateToken>();
-  var sseCompleter = Completer<SSE>();
-
-  receivePort.listen((message) {
-    if (message is RootIsolateToken) {
-      tokenCompleter.complete(message);
-    } else if (message is SSE) {
-      sseCompleter.complete(message);
-    }
-  });
-
-  return Tuple2(await tokenCompleter.future, await sseCompleter.future);
-}
-
-Future<SendPort> waitForSendPort(ReceivePort receivePort) {
-  Completer<SendPort> completer = Completer<SendPort>();
-
-  receivePort.listen((message) {
-    if (message is SendPort) {
-      completer.complete(message);
-    }
-  });
-
-  return completer.future;
-}
 
 Future<void> startListeningForSSE() async {
   const deviceId = String.fromEnvironment("device_id");
