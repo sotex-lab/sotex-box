c0265a9eda26cacea17fcb7e06e1712249ae9449
AleksaBajat <aleksabajat2001@gmail.com>
2024-08-13 16:10:10 +0200

    changed directories for logs and files

--- launcher/lib/src/database/storage.dart
+++ launcher/lib/src/database/storage.dart
@@ -4,12 +4,9 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DirectoryGetter {
-  Future<String> _applicationDirectory() async {
-    return (await getApplicationSupportDirectory()).path;
-  }
-
   Future<String> getMediaDirectory() async {
-    Directory applicationDirectory = Directory(await _applicationDirectory());
+    Directory applicationDirectory =
+        Directory((await getApplicationDocumentsDirectory()).path);
 
     final mediaDirectoryPath = join(applicationDirectory.path, "media");
 
@@ -23,7 +20,8 @@ class DirectoryGetter {
   }
 
   Future<String> getLogDirectory() async {
-    Directory applicationDirectory = Directory(await _applicationDirectory());
+    Directory applicationDirectory =
+        Directory((await getApplicationSupportDirectory()).path);
     final logDirectoryPath = join(applicationDirectory.path, "logs");
     final logDir = Directory(logDirectoryPath);
 
